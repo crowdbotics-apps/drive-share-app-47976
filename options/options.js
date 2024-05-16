38c5e0df8c57530483e937e253d1f35b05e9f364
Crowdbotics <team@crowdbotics.com>
2024-05-16 00:11:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "drive-share-app-47976",
+  url: "https://drive-share-app-47976.botics.co",
+  api: "https://drive-share-app-47976.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
